d3a73bc76f6876ccf306ba9e57d8c1468f822288
Rodrigo Monterrosa <20220377@ricaldone.edu.sv>
2024-06-11 15:14:22 -0600

    programaciòn spinner

--- app/src/main/java/bryan/miranda/ejemplospinner/pacientes.kt
+++ app/src/main/java/bryan/miranda/ejemplospinner/pacientes.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
+import modelo.dataClassDoctores
 
 class pacientes : Fragment() {
 
@@ -18,7 +26,45 @@ class pacientes : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val root = inflater.inflate(R.layout.fragment_pacientes, container, false)
 
-        return inflater.inflate(R.layout.fragment_pacientes, container, false)
+        //Mando a llamar a el spinner para programarlo
+        val spDoctores = root.findViewById<Spinner>(R.id.spDoctores)
+
+        //1- Creamos la funcion que haga un select
+        fun obtenerDoctores(): List<dataClassDoctores> {
+            //Creo un objeto de la clase conexion
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            //
+            val statement = objConexion?.createStatement()
+            val resultSet = statement?.executeQuery("select * from tbDoctores")!!
+
+            val listaDoctores = mutableListOf<dataClassDoctores>()
+
+            while (resultSet.next()){
+                val uuid = resultSet.getString("DoctorUUID")
+                val nombre = resultSet.getString("nombreDoctor")
+                val especialidad = resultSet.getString("Especialidad")
+                val telefono = resultSet.getString("Telefono")
+                val unDOctorCompleto = dataClassDoctores(uuid, nombre, especialidad, telefono)
+                listaDoctores.add(unDOctorCompleto)
+            }
+            return listaDoctores
+        }
+
+        //ultimo punto programar el spinner
+        CoroutineScope(Dispatchers.IO).launch {
+            //1- obtengo el listado de datos a mostrar
+            val listadoDoctores = obtenerDoctores()
+            val nombreDoctores = listadoDoctores.map { it.nombreDoctor }
+            withContext(Dispatchers.Main){
+                val miAdaptador = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, nombreDoctores)
+                spDoctores.adapter = miAdaptador
+            }
+        }
+
+
+        return root
     }
 }
